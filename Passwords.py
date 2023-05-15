3f63a3548af4655a013a44b451d960494844ebc2
FallenStars32 <133399254+FallenStars32@users.noreply.github.com>
2023-05-15 05:38:02 -0500

    Create Passwords.py

---
+++ Passwords.py
@@ -0,0 +1,5 @@
+
+email = "Enter your email here"
+Password = "Enter your SMTP Password here'
+
+'''You can use this link to learn how to add a password https://support.google.com/mail/answer/185833?hl=en"
